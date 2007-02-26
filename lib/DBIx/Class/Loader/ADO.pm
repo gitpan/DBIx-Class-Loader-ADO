f591ca84502851d5849fcb8658723dffe74c8e2f
Brian Cassidy <bricas@cpan.org>
2007-02-26 14:22:47 +0000

    Import of BRICAS/DBIx-Class-Loader-ADO-0.07 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Loader-ADO
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         BRICAS/DBIx-Class-Loader-ADO-0.07.tar.gz
    gitpan-cpan-author:       BRICAS
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/Loader/ADO.pm
+++ lib/DBIx/Class/Loader/ADO.pm
@@ -5,7 +5,7 @@ use base qw( DBIx::Class::Loader::Generic );
 
 use Carp;
 
-our $VERSION = '0.05';
+our $VERSION = '0.07';
 
 =head1 NAME
 
@@ -117,7 +117,7 @@ sub _table_info {
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2006 by Brian Cassidy
+Copyright 2007 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
